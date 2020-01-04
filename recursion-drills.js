e0b6705b64983666936832df0e0dee8027cb41c3
Ben Grabowski <gbowski30@gmail.com>
2020-01-04 13:54:00 -0700

    started question 1

--- recursion-drills.js
+++ recursion-drills.js
@@ -1,2 +1,14 @@
 //1. Counting Sheep
 
+const sheepCount = function(num) {
+    let counter = num;
+    
+    if (counter === 0) {
+        return `All sheep jumped over the fence`
+    } else {
+        counter - 1
+        return `${sheepCount(num)}: Another sheep jumps over the fence`
+    }
+}
+
+console.log(sheepCount(3));
